f4a8824a6cf499b35b589ed7e70f06b08355cb81
Carlene Cannon-Conner <carlene.cannon.conner@gmail.com>
2020-02-13 11:00:03 +0000

    Begin using a reducer to dispatch actions

--- src/App.js
+++ src/App.js
@@ -1,38 +1,59 @@
-import React, { useState } from 'react';
+import React, { useState, useReducer, useEffect } from 'react';
 import './App.scss';
 
-function App() {
+const defaultState =  { items: [] };
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'addItem':
+      return { 
+        items: [ 
+          ...state.items, 
+          {  
+            key: Date.now(),
+            value: action.payload
+          } 
+        ]
+      };
+
+      case 'removeItem':
+        return { 
+          items: state.items.filter(item => item.key !== action.payload)
+        };
+    default:
+      throw new Error();
+  }
+}
+const App = () => {
   const [newItem, setNewItem] = useState("");
-  const [items, setItems] = useState([])
-
-  function onChangeHandler(e) {
-    setNewItem(e.target.value)
-  }
+  const [state, dispatch] = useReducer(reducer, defaultState);
   
-  function addItem () {
-    setItems([...items, {
-      key: Date.now(),
-      value: newItem
-    }])
-    setNewItem("")
+  const onChangeHandler = (e) => setNewItem(e.target.value)
 
-  }
+  useEffect(() => {
+    console.table(state.items);
+    setNewItem('')
+  }, [state])
+
 
   return (
     <div className="c-app">
       <h1>TO DO App</h1>
       <input value={newItem} onChange={onChangeHandler} />
       <button 
-        onClick={addItem} 
+        onClick={() => dispatch({ type: 'addItem', payload: newItem })} 
         disabled={newItem === ""}
       >
         Add New Item
       </button>
       <ul>
-      {items.map(item => <li key={item.key}>{item.value}</li>)}
+      {state.items.map(item => (
+      <li key={item.key}>{item.value}
+        <button onClick={()=> dispatch({ type: 'removeItem', payload: item.key })}>X</button>
+      </li>
+      ))}
     </ul>
     </div>
   );
 }
-
 export default App;
